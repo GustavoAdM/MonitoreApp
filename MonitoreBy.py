e18049cf3a75de4994e08fc47a060092a7feb920
Gustavo Melo <guh.almeida39@gmail.com>
2025-04-29 09:41:24 -0300

    refat

--- MonitoreBy.py
+++ MonitoreBy.py
@@ -22,7 +22,7 @@ menu = st.navigation([st.Page(acompanhamento, title="Acompanhamento"),
                       st.Page(auditoria, title="Auditoria"), 
                       st.Page(view_processo, title="View Separação"),
                       st.Page(estoque_wms, title="Estoque WMS"),
-                      st.Page(capturar_ip, title="Capturar IP"),
+                      #st.Page(capturar_ip, title="Capturar IP"),
                       #st.Page(gerar_audio, title="Gerar Audio"),
                       ])
 menu.run()
