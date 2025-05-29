c036b62c3ca739f1a2e580c1f671f6618e2f3895
Gustavo Melo <guh.almeida39@gmail.com>
2025-05-29 15:15:40 -0300

    refatorado

--- MonitoreBy.py
+++ MonitoreBy.py
@@ -3,7 +3,6 @@ from PagesTemplate.Auditoria import auditoria
 from PagesTemplate.Processo import acompanhamento
 from PagesTemplate.ViewProcesso import view_processo
 from PagesTemplate.estoqueWMS import estoque_wms
-from PagesTemplate.Monitoramento_ti import monitoramento_ti
 import warnings
 
 warnings.simplefilter(action='ignore', category=UserWarning)
@@ -22,7 +21,5 @@ menu = st.navigation([st.Page(acompanhamento, title="Acompanhamento"),
                       st.Page(auditoria, title="Auditoria"), 
                       st.Page(view_processo, title="View Separação"),
                       st.Page(estoque_wms, title="Estoque WMS"),
-                      #st.Page(monitoramento_ti, title="Monitoramento TI"),
-                      #st.Page(gerar_audio, title="Gerar Audio"),
                       ])
 menu.run()
